178f8a0379bef0d9c64881c34e11680973bcd1bc
vishwa9011 <vishu842301@gmail.com>
2024-11-06 10:02:03 +0530

    Refactor code style for consistency and readability across multiple files

--- src/app/(Marketing)/layout.tsx
+++ src/app/(Marketing)/layout.tsx
@@ -1,14 +1,10 @@
 import React from "react";
 
-export default function MarketingLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <div className="selection:bg-[hsl(320,65%,52%,20%)]">
-      SKFNAOIKSDm
-      {children}
-    </div>
-  );
+export default function MarketingLayout({ children }: { children: React.ReactNode }) {
+   return (
+      <div className="selection:bg-[hsl(320,65%,52%,20%)]">
+         SKFNAOIKSDm
+         {children}
+      </div>
+   );
 }
