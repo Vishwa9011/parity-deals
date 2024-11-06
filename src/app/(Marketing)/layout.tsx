3812328fcb301cb35feff20cdfb7fcbeb591a531
vishwa9011 <vishu842301@gmail.com>
2024-11-06 09:56:48 +0530

    init

---
+++ src/app/(Marketing)/layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function MarketingLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="selection:bg-[hsl(320,65%,52%,20%)]">
+      SKFNAOIKSDm
+      {children}
+    </div>
+  );
+}
